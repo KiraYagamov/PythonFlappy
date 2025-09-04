c6daee203d516b864de1d2b93bf0b4a008153f60
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2025-09-04 16:27:19 +0300

    last update

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Initialize libs
 from engine import GameController, config, SceneLoader
-
 
 GameController.create_game(config.screen_size, "Flappy bird", lambda: SceneLoader.load_scene("MenuScene"))
